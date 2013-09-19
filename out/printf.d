cc2c280d2dc685fe6ea006a3e30ae4218da73d0e
Steve Parker <steven@stevenparker.com>
2013-09-18 22:25:33 -0400

    Removing output dir

--- out/printf.d
+++
@@ -1,6 +0,0 @@
-out/printf.o: printf.c printf.h \
- /Users/stevenparker/toolchains/gcc-arm-none-eabi-4_7-2013q2/bin/../lib/gcc/arm-none-eabi/4.7.4/include/stdarg.h
-
-printf.h:
-
-/Users/stevenparker/toolchains/gcc-arm-none-eabi-4_7-2013q2/bin/../lib/gcc/arm-none-eabi/4.7.4/include/stdarg.h:
